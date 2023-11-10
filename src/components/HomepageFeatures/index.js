2c21af2b6282f189808e3b67a3c545fc89aee50f
power4-DaCa <david.cai@power4-its.de>
2023-11-10 16:48:55 +0100

    2

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,7 +6,7 @@ const FeatureList = [
   {
 
 
-    title: 'Easy to Use314 2 33rrreee ddefeefeefee',
+    title: 'Easy to Use314',
 
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
