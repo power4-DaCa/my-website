8b9e773107c43f538770f6ec54d2df95fc0de31b
David Cai <davidcai2011@gmail.com>
2023-11-10 15:17:26 +0100

    main changed

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use1',
+    title: 'Easy to Use3',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
